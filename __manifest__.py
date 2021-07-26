11bacb3b595e3c1640a073c197f2cdff7314adbd
lukeaupiais <luke.aupiais@gmail.com>
2021-07-26 12:30:24 -0300

    Added Views with first menus.

--- __manifest__.py
+++ __manifest__.py
@@ -4,5 +4,10 @@
     'name': 'LukeTest',
     'installable': True,
     'application': True,
-    'auto_install': False
+    'auto_install': False,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ]
 }
